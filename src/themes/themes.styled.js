6f7e539d3f8e60371e886dc90ae30b4ff71f9aea
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-09-02 11:59:23 -0700

    Add dark/light theme

---
+++ src/themes/themes.styled.js
@@ -0,0 +1,17 @@
+export const light = {
+    name: "light",
+    background: "#FFFFFF",
+    color: {
+      markdownbody: "#35393F",
+      previewbody: "#35393F",
+    },
+  };
+  
+  export const dark = {
+    name: "dark",
+    background: "#151619",
+    color: {
+        markdownbody: "#C1C4CB",
+        previewbody: "#C1C4CB",
+    }
+  };
