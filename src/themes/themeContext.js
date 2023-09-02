686d4eae31e8883b00eaaff8684da5290a49a6a2
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-09-02 16:03:08 -0700

    Add theme testing,fix console errors

---
+++ src/themes/themeContext.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, createContext } from "react";
+import themes from "./themes";
+import PropTypes from "prop-types";
+
+export const ThemeContext = createContext(themes.light);
+
+/* eslint-disable react/prop-types */
+
+const ThemeContextWrapper = ({ children }) => {
+  const [theme, setTheme] = useState(themes.light);
+
+  useEffect(() => {
+    const currentTheme = JSON.parse(localStorage.getItem("theme"));
+    if (currentTheme) {
+      setTheme(currentTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+  }, [theme]);
+
+  console.log("theme");
+  console.log([theme]);
+  
+  console.log("boolean");
+  console.log("If theme name equals light " + theme.name === themes.light.name);
+
+  /* If the theme name is light, set it to dark. Else, set it to light */
+
+  const handleThemeChange = () => {
+    theme.name === themes.light.name
+      ? setTheme(themes.dark)
+      : setTheme(themes.light);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, handleThemeChange }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+ThemeContextWrapper.protoTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ThemeContextWrapper;
---
+++ src/themes/themes.js
@@ -0,0 +1,22 @@
+const themes = {
+    light: {
+      name: "light",
+      background: "#FFFFFF",
+      color: {
+        markdownbody: "#35393F",
+        previewbody: "#35393F",
+        cool: "light mode",
+      },
+    },
+    dark: {
+      name: "dark",
+      background: "#151619",
+      color: {
+        markdownbody: "#C1C4CB",
+        previewbody: "#C1C4CB",
+        cool: "dark mode",
+      },
+    },
+  };
+  
+  export default themes;
