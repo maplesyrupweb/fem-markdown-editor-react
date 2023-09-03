bf419b7a643fecb62ceab1bb14395cff4ee104e1
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-09-02 17:04:33 -0700

    Update theme background and color

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -4,8 +4,13 @@ import styled from "styled-components";
 const StyledNavbar = styled.div`
   width: 100vw;
   height: 72px;
-  border-bottom: 1px solid grey;
+  border-bottom: 1px solid #ffcc00;
 `;
+
+/*  Navbar = function() {
+  return <StyledNavbar></StyledNavbar>;
+}
+*/
 
 const Navbar = () => {
   return <StyledNavbar></StyledNavbar>;
--- src/themes/themes.js
+++ src/themes/themes.js
@@ -1,20 +1,30 @@
 const themes = {
     light: {
       name: "light",
-      background: "#FFFFFF",
+      background: {
+        main: "#FFFFFF",
+        sectionheader: "#F5F5F5",
+        blockquote: "979797",
+      }, 
       color: {
         markdownbody: "#35393F",
-        previewbody: "#35393F",
-        cool: "light mode",
+        previewbody: "#7C8187",
+        previewheader: "#35393F",
+        text: "light mode"
       },
     },
     dark: {
       name: "dark",
-      background: "#151619",
+      background: {
+        main: "#151619",
+        sectionheader: "#F5F5F5",
+        blockquote: "979797",
+      },
       color: {
         markdownbody: "#C1C4CB",
         previewbody: "#C1C4CB",
-        cool: "dark mode",
+        previewheader: "#FFFFFF",
+        text: "dark mode"
       },
     },
   };
